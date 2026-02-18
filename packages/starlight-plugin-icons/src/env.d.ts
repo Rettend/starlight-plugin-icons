fe4031966b1f4d82b7852c6e7202272ae96d763e
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2026-02-19 00:25:53 +0100

    fix: add translation key to sidebar

--- packages/starlight-plugin-icons/src/env.d.ts
+++ packages/starlight-plugin-icons/src/env.d.ts
@@ -2,9 +2,3 @@ declare namespace App {
   type StarlightLocals = import('@astrojs/starlight').StarlightLocals
   interface Locals extends StarlightLocals {}
 }
-
-declare namespace StarlightApp {
-  interface I18n {
-    [key: string]: string
-  }
-}
